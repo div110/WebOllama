fd20d0edd720a52153a575684bc5c18e38e572d0
div110 <davidxhalamka@gmail.com>
2025-02-25 23:18:59 +0100

    Add files via upload

---
+++ homellama.py
@@ -0,0 +1,28 @@
+form flask import Flask, request, render_template
+import ollama
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/ollama', methods=['POST'])
+def query_ollama():
+    user_input = request.form.get("input")
+    if user_input:
+        try:
+            response = ollama.chat(
+                    model="deepseek-r1:1.5b"  #can modify this
+                    messages=[{"role":"user","content": user_input}]
+                )
+            mode_response = response['message']['content']
+
+            return render_template('index.html', response=model_response, user_input=user_input)
+        except Exception as e:
+            return render_template('index.html',error=str(e))
+        return render_template('index.html',error="no input!!")
+
+if __name__="__main__":
+    app.run(debug=True, host="127.0.0.1",port=5000) #can modify this
